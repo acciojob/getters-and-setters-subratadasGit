ab8a8b35dfefd37fbd123411f74c81536d452edd
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2026-02-08 01:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name , age){
+		this._name = name ;
+		this._age= age;
+	}
+	get name(){
+		return this._name ;
+	}
 
-class Student extends Person {}
+	set age(){
+		return this._age
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	 study(){
+		return `${this._name} is studying`
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		return `${this._name} is teaching`
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
